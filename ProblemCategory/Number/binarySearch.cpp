58c64876d34b83fca9fc72920ab1171b1a965ff6
Triste <1182511338@qq.com>
2017-04-25 22:54:00 +0800

    add binarySearch

---
+++ ProblemCategory/Number/binarySearch.cpp
@@ -0,0 +1,19 @@
+//二分查找,有两种
+//1.lower_bound:找到第一个>=x的下标
+//2.upper_bound:找到第一个>x的下标
+
+int main(){
+	int a[] = { 1, 2, 2, 4, 4, 4, 5, 6, 7 };
+	int left = 0, right = 8;
+	int x = 3;
+	int mid;
+	while (left < right){
+		mid = left + ((right - left) >> 1);
+		if (a[mid] > x)right = mid;// >= lower bound,the first num >= x ;
+									// > upper bound ,the first num > x
+		else left = mid+1;
+	}
+	cout << left;
+	system("pause");
+	return 0;
+}
---
+++ Problems/binarySearch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a[] = { 1, 2, 2, 4, 4, 4, 5, 6, 7 };
+	int left = 0, right = 8;
+	int x = 3;
+	int mid;
+	while (left < right){
+		mid = left + ((right - left) >> 1);
+		if (a[mid] > x)right = mid;// >= lower bound,the first num >= x ;
+									// > upper bound ,the first num > x
+		else left = mid+1;
+	}
+	cout << left;
+	system("pause");
+	return 0;
+}
+
