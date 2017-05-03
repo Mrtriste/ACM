0acbb35e9c3cb315815e7f6775bd7133f705363a
MrTriste <jinchengwone@gmail.com>
2017-05-03 20:00:38 +0800

    add dijkstra

--- ProblemCategory/ShortestPath/Dijkstra.cpp
+++ ProblemCategory/ShortestPath/Dijkstra.cpp
@@ -15,7 +15,73 @@
 showPath()中的点都是根据S中的点一点一点更新来的 path[i]=v表示到i的最短路径i之前紧接的点为v
 */
 
+///////////////////////////////////////////////////////////////////////
+//priority_queue version
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
 
+struct Node2{
+    int v;
+    int len;
+    Node2(int v, int len) :v(v), len(len){}
+    bool operator<(const Node2& node)const{
+        return len > node.len;
+    }
+};
+
+int main(){
+    const int N = 100;
+
+    vector<Node2> Graph[N];
+    int a1[] = { 0, 0, 1, 1, 1, 2, 3, 3, 4, 5, 4 };
+    int a2[] = { 1, 3, 3, 2, 4, 4, 4, 5, 5, 6, 6 };
+    int a3[] = { 7, 5, 9, 8, 7, 5, 15, 6, 8, 11, 9 };
+    for (int i = 0; i < 11; ++i){
+        Graph[a1[i]].push_back(Node2(a2[i], a3[i]));
+        Graph[a2[i]].push_back(Node2(a1[i], a3[i]));
+    }
+    
+    bool isVisit[N];
+    int dis[N];
+    memset(isVisit, false, sizeof(isVisit));
+    memset(dis, 100000000, sizeof(dis));
+    for (int i = 0; i < N; ++i)
+        dis[i] = 10000000;
+    int start = 2;
+    dis[start] = 0;
+    //isVisit[start] = true;//note:can't set here
+    priority_queue<Node2> que;
+    que.push(Node2(start, 0));
+    while (!que.empty()){
+        int v = que.top().v, l = que.top().len; que.pop();//this is the prodecue that find the shortest path length amoung the {v-s}
+        if (isVisit[v])continue;
+        isVisit[v] = true;
+        //the new vertex(we call it ver) which joins the set s
+        //for the the vertexs which connects with ver,we update the path which pass ver if it is shorter
+        for (int i = 0; i < Graph[v].size(); ++i){
+            int len = Graph[v][i].len;//edge(v,v2)
+            int v2 = Graph[v][i].v;
+            if (!isVisit[v2] && dis[v2] > dis[v] + len){
+                dis[v2] = dis[v] + len;
+                que.push(Node2(v2, dis[v2]));//note:dis[v2] is the path length from start to v2
+            }
+        }
+    }
+
+    for (int i = 0; i < 7; ++i)
+        cout << dis[i] << endl;
+
+
+    system("pause");
+    return 0;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////
+//normal version
 #include <iostream>
 #include <stack>
 #define M 100
