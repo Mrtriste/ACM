82e0bcc16e677c301024d1ba06b0656dda4db01c
MrTriste <jinchengwone@gmail.com>
2017-05-26 22:48:40 +0800

    add 1074

---
+++ HDU/HDU/1074.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+	char name[100];
+	int d;
+	int c;
+};
+
+int main2(){
+	int T; cin >> T;
+	for (int e = 0; e < T; ++e){
+		int n; cin >> n;
+		for (int i = 1; i <= n; ++i){
+
+		}
+	}
+	return 0;
+}
