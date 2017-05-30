b9e1db3536a7eec895ed7ea0af6660912cff1962
MrTriste <jinchengwone@gmail.com>
2017-05-30 22:51:54 +0800

    add 2102

---
+++ HDU/HDU/2102.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int a[12][12];
+int b[12][12];
+int m, n,T;
+
+int main(){
+	int C; cin >> C;
+	char s[12];
+	while (C--){
+		cin >> n >> m >> T;
+		int i, j;
+		for (i = 0; i < n; ++i){
+			cin >> s;
+			for (j = 0; j < m; ++j){
+
+			}
+		}
+			
+	}
+}
