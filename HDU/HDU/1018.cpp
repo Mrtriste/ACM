ecae86f7277a53db7e40bf765b3c7c2cf0259362
MrTriste <jinchengwone@gmail.com>
2017-06-02 22:48:12 +0800

    add 1018

--- HDU/HDU/1018.cpp
+++ HDU/HDU/1018.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
-const int maxSize = 100;
-struct bigInt{
-
-};
-
-int main1(){
+int main(){
+	int T; cin >> T;
+	while (T--){
+		int n; cin >> n;
+		double cnt = 0;
+		for (int i = 1; i <= n; ++i)
+			cnt += (log10(i));
+		cout <<int(cnt)+1<< endl;
+	}
 	return 0;
 }
