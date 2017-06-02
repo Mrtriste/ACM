820d9df128a3e77f57ef3368d1e71d3af0b97f37
MrTriste <jinchengwone@gmail.com>
2017-06-02 22:47:47 +0800

    add 1002

--- HDU/HDU/1002.cpp
+++ HDU/HDU/1002.cpp
@@ -10,20 +10,22 @@ char b[1010];
 struct bigInteger{
 	int digit[MAX];
 	int size;
-	void init(){
+
+	bigInteger(){
 		for (int i = 0; i < MAX; ++i){
 			digit[i] = 0;
 		}
 		size = 0;
 	}
+
 	void set(char s[]){         //用一个小整数来设置高精度整数  
-		init();
-		int l = strlen(s);
-		for (int i = l - 1, j = 0, t = 0, c = 1; i >= 0; --i){
+		int b = 0, l = strlen(s) - 1;
+		while (s[b] == '0'&&b + 1 <=l)++b;
+		for (int i = l, j = 0, t = 0, c = 1; i >= b; --i){
 			t += (s[i] - '0') * c;
 			j++;
 			c *= 10;
-			if (j == 4 || i == 0){
+			if (j == 4 || i == b){
 				digit[size++] = t;
 				j = 0;  t = 0;  c = 1;
 			}
@@ -31,7 +33,6 @@ struct bigInteger{
 	}
 	bigInteger operator + (const bigInteger &A) const{
 		bigInteger ret;
-		ret.init();
 		int carry = 0;
 		for (int i = 0; i < size || i < A.size; ++i){
 			int t = digit[i] + A.digit[i] + carry;
@@ -46,8 +47,9 @@ struct bigInteger{
 	}
 
 	void print(){
-		printf("%d", digit[size - 1]);
-		for (int i = size - 2; i >= 0; --i)
+		int l = size-1;
+		printf("%d", digit[l]);
+		for (int i = l-1; i >= 0; --i)
 			printf("%04d", digit[i]);
 		cout << endl;
 	}
@@ -55,14 +57,10 @@ struct bigInteger{
 
 ostream& operator<<(ostream& out, bigInteger& b)   //重载输出运算符
 {
-	int i;
-	cout << b.digit[b.size - 1];
-	for (i = b.size - 2; i >= 0; i--)
-	{
-		cout.width(4);
-		cout.fill('0');
-		cout << b.digit[i];
-	}
+	int l = b.size - 1;
+	cout << b.digit[l];
+	for (int i = l - 1; i >= 0; i--)
+		printf("%04d", b.digit[i]);
 	return out;
 }
 
@@ -76,8 +74,7 @@ int main(){
 		x.set(a), y.set(b);
 		x = x + y;
 		printf("Case %d:\n", cnt);
-		cout << a << " + " << b << " = " << x;
-		//x.print();
+		cout << a << " + " << b << " = " << x << endl;
 		if (cnt != T)cout << endl;
 	}
 }
