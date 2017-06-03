5937654f89d6572a16696ef98c640213a604a5f5
MrTriste <jinchengwone@gmail.com>
2017-06-04 00:14:32 +0800

    add 2050

---
+++ HDU/HDU/2050.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	long long a[10010];
+	a[1] = 2;
+	for (int i = 2; i <= 10000; ++i)
+		a[i] = a[i - 1] + 4 * i - 3;
+	int n, T; cin >> T;
+	while (T--){
+		cin >> n;
+		cout << a[n] << endl;
+	}
+		
+}
