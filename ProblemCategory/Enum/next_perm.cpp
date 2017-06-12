f14e71d65a63a3b6781d265774f5be81f6652d3b
MrTriste <jinchengwone@gmail.com>
2017-06-12 20:17:49 +0800

    add next_perm.cpp

---
+++ ProblemCategory/Enum/next_perm.cpp
@@ -0,0 +1,42 @@
+//poj1256 下一个排列
+//'A'<'a'<'B'<'b'<...<'Z'<'z'
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool smaller(char a, char b){
+	if (tolower(a) != tolower(b))
+		return tolower(a) < tolower(b);
+	return a < b;
+}
+
+bool next(char*first, char* last){
+	if (first == last || first == last - 1)//0 or 1 element
+		return false;
+	char *i = last - 1;
+	while (1){
+		char *ii = i;
+		i--;
+		if (smaller(*i, *ii)){
+			char *j = last;
+			while (!smaller(*i, *--j));
+			swap(*i, *j);
+			reverse(ii,last);//last,reverse ii to the last
+			return true;
+		}
+		if (i == first)return false;
+	}
+}
+
+int main(){
+	char a[20];
+	int T; cin >> T;
+	while (T--){
+		cin >> a;
+		sort(a, a + strlen(a),smaller);
+		printf("%s\n", a);
+		while (next(a,a+strlen(a)))
+			printf("%s\n", a);
+	}
+}
