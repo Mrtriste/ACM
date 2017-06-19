561fd85c32898c1e9f1cb5ab5c7bfe10f1d71f88
MrTriste <jinchengwone@gmail.com>
2017-06-19 20:00:21 +0800

    add hdu 1045

---
+++ HDU/HDU/1045.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//using namespace std;
+//
+//int n;
+//char a[6][6];
+//bool vis[6][6];
+//bool col[6];
+//
+//void DFS(){
+//
+//}
+//
+//int main1(){
+//	char s[6];
+//	while (cin >> n&&n){
+//		memset(vis, 0, sizeof(vis));
+//		memset(col, 0, sizeof(col));
+//		for (int i = 0; i < n; ++i)
+//			cin >> s;
+//
+//		
+//	}
+//	return 0;
+//}
