cfc97267378cfd4efd6ea165980cf9ff36db09ae
MrTriste <jinchengwone@gmail.com>
2017-06-27 23:45:37 +0800

    add HDU1520

---
+++ HDU/HDU/1520.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+struct Node{
+	int par, son, bro, present, not_present;
+	void init(){
+		par = son = bro = not_present = 0;
+	}
+
+	int max(){
+		return present > not_present ? present : not_present;
+	}
+};
+
+Node node[6010];
+
+void DFS(int r){
+	int son = node[r].son;
+	while (son){
+		DFS(son);
+		node[r].present += node[son].not_present;
+		node[r].not_present += node[son].max();
+		son = node[son].bro;
+	}
+}
+
+int main(){
+	while (cin >> n){
+		for (int i = 1; i <= n; ++i){
+			scanf("%d", &node[i].present);
+			node[i].init();
+		}
+		int par, son;
+		while (scanf("%d%d", &son, &par) && (par + son)){
+			node[son].par = par;
+			node[son].bro = node[par].son;
+			node[par].son = son;
+		}
+		int root;
+		for (int i = 1; i <= n; ++i){
+			if (!node[i].par){
+				root = i;
+				DFS(i);
+				break;
+			}
+		}
+		cout << node[root].max() << endl;
+	}
+}
