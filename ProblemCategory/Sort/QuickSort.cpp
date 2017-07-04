1ebaee2464a5773940539fbe07df0af3e34a5666
MrTriste <jinchengwone@gmail.com>
2017-07-04 23:23:32 +0800

    modify QuickSort.cpp

--- ProblemCategory/Sort/QuickSort.cpp
+++ ProblemCategory/Sort/QuickSort.cpp
@@ -14,11 +14,11 @@ void quick_sort_recursive(T arr[], int start, int end) {
 	//上面的循环什么时候结束？
 	//1.left = right and arr[left]>mid
 	//2.left = right and left = end-1
-	//1.其实有两种 1.1 left贴right,在一次swap之后,arr[right]>mid & left--,then left = right,
-	//             1.2 right贴left,left--到arr[left]>mid停住,right--到right=left
+	//1.其实有两种 1.1 left贴right,在一次swap之后,arr[right]>mid & left++,then left = right,
+	//             1.2 right贴left,left++到arr[left]>mid停住,right--到right=left
 	//2.在刚进入这个递归，left一路left++，到left=right停下，此时left=right=end-1
 
-	if (arr[left] >= arr[end])//针对1
+	if (arr[left] >= arr[end])//针对1，这里是>=，而不是>,
 		swap(arr[left], arr[end]);
 	else//针对2
 		left++;
@@ -41,7 +41,7 @@ void quick_sort(int s[], int l, int r)
     if (l < r)  
     {  
         //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1  
-        int i = l, j = r, x = s[l];  
+        int i = l, j = r, x = s[l];  //先把左边的挖了，所以先从右往左找
         while (i < j)  
         {  
             while(i < j && s[j] >= x) // 从右向左找第一个小于x的数  
@@ -52,10 +52,10 @@ void quick_sort(int s[], int l, int r)
             while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数  
                 i++;    
             if(i < j)   
-                s[j--] = s[i];  
-        }  
+                s[j--] = s[i];
+        }
         s[i] = x;  
-        quick_sort(s, l, i - 1); // 递归调用   
+        quick_sort(s, l, i - 1); // 递归调用
         quick_sort(s, i + 1, r);  
     }  
 }
