d99604cea78e8e856b9064f326ce37f67b8e772b
MrTriste <jinchengwone@gmail.com>
2017-07-06 23:21:40 +0800

    modify kruskal.cpp

--- ProblemCategory/MinSpanTree/kruskal.cpp
+++ ProblemCategory/MinSpanTree/kruskal.cpp
@@ -1,83 +1,65 @@
 //克鲁斯卡尔算法——最小生成树
 //核心思想是将图中的每条边排序，每次选一个最小的边，看加入这条边后是否会和已经选的那些点构成环，如果不会的话就将这条边的两个点加入集合
 //至于如何判断环，用并查集
+//HDU1233
 
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-const int MAX_SIZE = 100;
-int parentK[MAX_SIZE];
-struct Edge{
-	int start;
-	int end;
-	int weight;
-	bool isSelect;
+struct Node{
+	int u, v, d;
+	Node(){}
+	Node(int uu, int vv, int dd) :u(uu), v(vv), d(dd){}
+	bool operator<(const Node& n){
+		return d < n.d;
+	}
 };
 
-int cmp(const Edge a, const Edge b){
-	if (a.weight < b.weight)
-		return 1;
-	else
-		return 0;
+int n;
+int par[110];
+Node edge[5010];
+
+void Make(){
+	for (int i = 1; i <= 110; ++i)
+		par[i] = i;
 }
 
-void makeSet(){
-	for (int i = 0; i < MAX_SIZE; ++i)
-		parentK[i] = i;
-}
-
-int findX(int x){
-	int root = x, t;
-	while (root != parentK[root])root = parentK[root];
-	
-	while (root != x){
-		t = parentK[x];
-		parentK[x] = root;
-		x = t;
+int find(int x){
+	int root = x, r;
+	while (root != par[root])root = par[root];
+	while (x != root){
+		r = par[x];
+		par[x] = root;
+		x = r;
 	}
 	return root;
 }
 
-void Union(int a,int b){
-	int x = findX(a), y = findX(b);
-	if (x != y)
-		parentK[x] = y;
+void Union(int x, int y){
+	int a = find(x), b = find(y);
+	if (a != b)
+		par[a] = b;
 }
 
 int main(){
-	Edge edge[MAX_SIZE];
-	int vtx[MAX_SIZE];
-	int edge_num = 11;
-	int vertex_num = 7;
-
-	//http://www.cnblogs.com/qianbixin/p/5005161.html
-	//use the graph of above link
-	int a1[] = { 0, 0, 1, 1, 1, 2, 3, 3, 4, 5, 4 };
-	int a2[] = { 1, 3, 3, 2, 4, 4, 4, 5, 5, 6, 6 };
-	int a3[] = { 7, 5, 9, 8, 7, 5, 15, 6, 8, 11, 9 };
-	for (int i = 0; i < edge_num; ++i){
-		edge[i].start = a1[i];
-		edge[i].end = a2[i];
-		edge[i].weight = a3[i];
-		edge[i].isSelect = false;
-	}
-	sort(edge, edge + edge_num, cmp);
-
-	makeSet();
-	for (int i = 0; i < edge_num; ++i){
-		int x = findX(edge[i].start), y = findX(edge[i].end);
-		if ( x!=y ){
-			Union(edge[i].start, edge[i].end);
-			edge[i].isSelect = true;
+	while (cin >> n&&n){
+		Make();
+		int a, b, l, cnt = 1,ans=0;
+		int m = n*(n - 1) / 2;
+		for (int i = 0; i < m; ++i){
+			scanf("%d%d%d", &a, &b, &l);
+			edge[i] = Node(a, b, l);
 		}
+		sort(edge, edge + m);
+		for (int i = 0; i < m; ++i){
+			int u = edge[i].u, v = edge[i].v, d = edge[i].d;
+			if (find(u) != find(v)){
+				Union(u, v);
+				ans += d;
+				if (++cnt == n)break;//there are n-1 edges at most
+			}
+		}
+		cout << ans << endl;
 	}
-		
-			
-	for (int i = 0; i < edge_num; ++i)
-		if (edge[i].isSelect)
-			cout <<edge[i].start<<" "<<edge[i].end<<" "<< edge[i].weight << endl;
-
-	system("pause");
-	return 0;
 }
